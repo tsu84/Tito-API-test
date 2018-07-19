b56b1848ac0f8ffaaf286eac3842860f790b2579
Tsuneo Kurihara <tsuneo.kurihara@gmail.com>
2018-07-19 09:49:55 +0200

    Discount Codes test file added

---
+++ tests/discountcodes.test.js
@@ -0,0 +1,26 @@
+import 'isomorphic-fetch'
+import { APIKEY, ENDPOINT, ACCOUNT, TESTEVENTNAME } from '../config'
+
+/*
+    http://teamtito.github.io/tito-api-docs/?shell#get-all-discount-codes
+    
+    curl --request GET \
+        --url 'https://api.tito.io/v2/an-account/awesome-conf/discount_codes' \
+        --header 'Authorization: Token token=YOUR-API-KEY' \
+        --header 'Accept: application/vnd.api+json' \
+*/
+test('"Get all Discount Codes" is working', () => {
+    const init = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token token=${APIKEY}`,
+            'Accept': 'application/vnd.api+json'
+        }
+    }
+    const url = `${ENDPOINT}/${ACCOUNT}/${TESTEVENTNAME}/discount_codes`
+        
+    expect.assertions(1)
+    return fetch(url, init).then(response => {
+        expect(response.ok).toBeTruthy()
+    })
+})
