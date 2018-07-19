47c893e5cf5f2cea638068e363fb748ac2af9ab5
Tsuneo Kurihara <tsuneo.kurihara@gmail.com>
2018-07-19 09:54:12 +0200

    Refunds test file added

---
+++ tests/refunds.test.js
@@ -0,0 +1,26 @@
+import 'isomorphic-fetch'
+import { APIKEY, ENDPOINT, ACCOUNT, TESTEVENTNAME } from '../config'
+
+/*
+    http://teamtito.github.io/tito-api-docs/?shell#get-all-refunds
+
+    curl --request GET \
+        --url 'https://api.tito.io/v2/an-account/awesome-conf/refunds' \
+        --header 'Authorization: Token token=YOUR-API-KEY' \
+        --header 'Accept: application/vnd.api+json' \
+*/
+test('"Get all Refunds" is working', () => {
+    const init = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token token=${APIKEY}`,
+            'Accept': 'application/vnd.api+json'
+        }
+    }
+    const url = `${ENDPOINT}/${ACCOUNT}/${TESTEVENTNAME}/refunds`
+        
+    expect.assertions(1)
+    return fetch(url, init).then(response => {
+        expect(response.ok).toBeTruthy()
+    })
+})
