076a2ea8138d91a8bad6eba811bbb65b640ca95b
Tsuneo Kurihara <tsuneo.kurihara@gmail.com>
2018-07-19 09:56:11 +0200

    Registrations test file added

---
+++ tests/registrations.test.js
@@ -0,0 +1,26 @@
+import 'isomorphic-fetch'
+import { APIKEY, ENDPOINT, ACCOUNT, TESTEVENTNAME } from '../config'
+
+/*
+    http://teamtito.github.io/tito-api-docs/?shell#get-all-registrations
+
+    curl --request GET \
+        --url 'https://api.tito.io/v2/an-account/awesome-conf/registrations' \
+        --header 'Authorization: Token token=YOUR-API-KEY' \
+        --header 'Accept: application/vnd.api+json' \
+*/
+test('"Get all Registrations" is working', () => {
+    const init = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token token=${APIKEY}`,
+            'Accept': 'application/vnd.api+json'
+        }
+    }
+    const url = `${ENDPOINT}/${ACCOUNT}/${TESTEVENTNAME}/registrations`
+        
+    expect.assertions(1)
+    return fetch(url, init).then(response => {
+        expect(response.ok).toBeTruthy()
+    })
+})
